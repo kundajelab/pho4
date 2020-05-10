aa0e785caad68ede10ded2a7b507ae2b89adf125
Amr <alexandari@users.noreply.github.com>
2020-05-10 14:52:07 -0500

    Update generateGif.py

--- generateGif.py
+++ generateGif.py
@@ -9,12 +9,12 @@ for task in os.listdir("insilico_exp/"):
         if repeat == ".DS_Store": continue
         for filename in os.listdir("insilico_exp/"+task+"/"+variation+"/"+repeat):
             if filename.endswith(".zip"):
-                subprocess.check_call(["tar", "xvf", "insilico_exp/"+task+"/"+repeat+"/"+filename])
+                subprocess.check_call(["tar", "xvf", "insilico_exp/"+task+"/"+variation+"/"+repeat+"/"+filename])
                 subprocess.check_call(["convert",
-                    "data/insilico_exp/"+task+"/"+repeat+"/"+filename.split('.')[0]+"/*.png",
+                    "data/insilico_exp/"+task+"/"+variation+"/"+repeat+"/"+filename.split('.')[0]+"/*.png",
                     filename.split('.')[0]+".gif"])
                 subprocess.check_call(["mv",
                     filename.split('.')[0]+".gif",
-                    "insilico_exp/"+task+"/"+repeat+"/"])
+                    "insilico_exp/"+task+"/"+variation+"/"+repeat+"/"])
                 subprocess.check_call(["rm", "-rf", "data"])
-                subprocess.check_call(["rm", "-rf", "insilico_exp/"+task+"/"+repeat+"/"+filename])
+                subprocess.check_call(["rm", "-rf", "insilico_exp/"+task+"/"+variation+"/"+repeat+"/"+filename])
