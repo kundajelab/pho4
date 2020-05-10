fa8c43a4bbd39e51324d7be13ef57d36138e2e55
Amr <alexandari@users.noreply.github.com>
2020-05-10 14:44:41 -0500

    Update generateGif.py

--- generateGif.py
+++ generateGif.py
@@ -1,11 +1,13 @@
 import os
 import subprocess
 
+variation = "distance"
+
 for task in os.listdir("insilico_exp/"):
     if task == ".DS_Store": continue
-    for repeat in os.listdir("insilico_exp/"+task):
+    for repeat in os.listdir("insilico_exp/"+task+"/"+variation):
         if repeat == ".DS_Store": continue
-        for filename in os.listdir("insilico_exp/"+task+"/"+repeat):
+        for filename in os.listdir("insilico_exp/"+task+"/"+variation+"/"+repeat):
             if filename.endswith(".zip"):
                 subprocess.check_call(["tar", "xvf", "insilico_exp/"+task+"/"+repeat+"/"+filename])
                 subprocess.check_call(["convert",
